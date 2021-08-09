cdadf52d279e232d1799e3880f0b63af0b4d8e01
11uha <if@adspend.ru>
2021-08-09 12:49:13 +0300

    main as example & readme

--- main.go
+++ main.go
@@ -13,27 +13,41 @@ func panicOnError(err error) {
 func main() {
 	var err error
 
-	c := InitCube(3, 3)
+	c3 := InitCube(3, 3)
+	c3.Print()
+	fmt.Println("\n---------------------------")
 
-	c.selected = 13
-	c.Print()
-	c.selected -= _Size
-	c.Print()
-	c.selected -= _Size
-	c.Print()
-	// fmt.Println("\n")
+	// c.selected = 13
+	// c.Print()
+	// c.selected -= _Size
+	// c.Print()
+	// c.selected -= _Size
+	// c.Print()
+	// // fmt.Println("\n")
 
-	//	err = c.Move([]int{0, 0, 1}, _Plane{directionY, directionX})
-	//	panicOnError(err)
-	//	c.Print()
-
-	//	err = c.Move([]int{0, 0, 1}, _Plane{directionX, directionY})
-	//	panicOnError(err)
-	//	c.Print()
-
-	err = c.Move([]int{0, 0, 0}, _Plane{directionX, directionZ})
+	err = c3.Move([]int{0, 0, 1}, _Plane{directionY, directionX})
 	panicOnError(err)
-	c.Print()
+	c3.Print()
+	fmt.Println("\n---------------------------")
+
+	err = c3.Move([]int{0, 0, 1}, _Plane{directionX, directionY})
+	panicOnError(err)
+	c3.Print()
+	fmt.Println("\n---------------------------")
+
+	err = c3.Move([]int{0, 0, 0}, _Plane{directionX, directionZ})
+	panicOnError(err)
+	c3.Print()
+	fmt.Println("\n---------------------------")
+
+	// 4d cube
+	c4 := InitCube(4, 3)
+	c4.Print()
+	fmt.Println("\n---------------------------")
+
+	c4.Move([]int{0, 0, 0, 0}, _Plane{directionX, directionY})
+	c4.Print()
+	fmt.Println("\n---------------------------")
 
 	fmt.Println("end")
 }
